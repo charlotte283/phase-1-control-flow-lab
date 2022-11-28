176918db69f26184e4c8a4c6914f17101d2e282b
charlotte karimi <[charlotte.karimi@student.moringaschool.com]>
2022-11-28 13:38:14 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+function scuberGreetingForFeet(feet=199){
+  if (feet<=400) {'freeSample'
+    return 'This one is on me!'
+    
+  } else if (feet>=2000) {'30dollars'
+        return 'I will gladly take your thirty bucks.'
+      
+      } else if (feet>2500) {'does not allow rides over 2500 feet'
+              return 'No can do.' 
+              } else {
+              
+            } {
+        
+      }{
+    
+  }
+ 
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+const city= 'Pittsburgh'
+function ternaryCheckCity(city='Pittsburgh]'){
+    
+       return ('NYC') ?'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (switchOnCharmFromTip) {
+    case 'generous': 
+          'Thank you so much.'
+      break;
+    case 'not as generous': 
+             'Thank you.'
+       break;
+    case 'thanks for everything':
+             'Bye'
+       
+  }
 }
